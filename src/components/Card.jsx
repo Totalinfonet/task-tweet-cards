5fafc7a592bfe0f8becd7967cd66c564acb2e80a
Totalinfonet <totalinfonet@gmail.com>
2023-05-09 13:35:16 +0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const Card = ({ user }) => {
   return (
     <div className="card">
       <img src={user.avatar} alt={`${user.user}'s avatar`} />
       <div className="card-body">
-        <h5 className="card-title">{user.user}</h5>
+        {/* <h5 className="card-title">{user.user}</h5> */}
         <p className="card-text">{`Tweets: ${user.tweets}`}</p>
         <p className="card-text">{`Followers: ${user.followers}`}</p>
       </div>
